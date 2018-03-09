16d0fffd47345d6c5b029e01803ad0a32b8021fe
Thomas William Page <tom.w.page@gmail.com>
2018-03-09 14:57:25 +0100

    Add a test for Tutorial model

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when creating a Tutorial' do
+    it 'saves correctly' do
+      Tutorial.create!(
+        name: 'Great Tutorial',
+        description: 'Just a fantastic one, awesome',
+        average_score: 8
+      )
+
+      expect(Tutorial.count).to eq 1
+      expect(Tutorial.first.name).to eq 'Great Tutorial'
+      expect(Tutorial.first.description).to eq 'Just a fantastic one, awesome'
+      expect(Tutorial.first.average_score).to eq 8
+    end
+  end
 end

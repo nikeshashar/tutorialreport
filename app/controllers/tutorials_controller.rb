64757a2b67cfbb831ef3db077bf946a8fcae2c50
Thomas William Page <tom.w.page@gmail.com>
2018-03-10 13:40:34 +0100

    Add minor front end enhancements

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,6 @@
 class TutorialsController < ApplicationController
-  before_action :authenticate_user!
-  
+  before_action :authenticate_user!, except: [ :index, :show ]
+
   def index
     @tutorials = Tutorial.all
   end

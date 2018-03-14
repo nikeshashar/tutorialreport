1434efdc5e186b6fc3b9307adc42cd143cbc0783
Thomas William Page <tom.w.page@gmail.com>
2018-03-14 22:33:00 +0100

    Add more links feature specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :user do
     trait :standard_user do
-      email 'an_email@email.com'
+      email 'standard_user@email.com'
       password '123456'
       admin false
     end
     trait :admin do
-      email 'an_email@email.com'
+      email 'admin@email.com'
       password '123456'
       admin true
     end
--- spec/features/check_links_spec.rb
+++ spec/features/check_links_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Correct links', type: feature do
   let(:user) { create(:user, :standard_user) }
-  let(:tutorial) { create(:tutorial) }
+  let(:admin) { create(:user, :admin) }
+  let!(:tutorial) { create(:tutorial) }
 
   describe 'when at root' do
     it 'shows tutorial link' do
@@ -11,6 +12,50 @@ RSpec.describe 'Correct links', type: feature do
       find(:xpath, "//a[@href='/tutorials']").click
 
       expect(page).to have_current_path('/tutorials')
+    end
+  end
+
+  describe 'when tutorial index as admin' do
+    it 'shows add, edit and delete links' do
+      visit '/'
+
+      find(:xpath, "//a[@href='/users/sign_in']").click
+
+      within '.new_user' do
+        fill_in 'user_email', with: admin.email
+        fill_in 'user_password', with: admin.password
+        find('[name=commit]').click
+      end
+
+      visit '/tutorials'
+      expect(page).to have_text('Add a tutorial')
+
+      find(:xpath, "//a[@href='/tutorials/#{tutorial.id}']").click
+
+      expect(page).to have_text('Edit this tutorial')
+      expect(page).to have_text('Delete this tutorial')
+    end
+  end
+
+  describe 'when tutorial index as user' do
+    it 'doesnt show add, edit and delete links' do
+      visit '/'
+
+      find(:xpath, "//a[@href='/users/sign_in']").click
+
+      within '.new_user' do
+        fill_in 'user_email', with: user.email
+        fill_in 'user_password', with: user.password
+        find('[name=commit]').click
+      end
+
+      visit '/tutorials'
+      expect(page).not_to have_text('Add a tutorial')
+
+      find(:xpath, "//a[@href='/tutorials/#{tutorial.id}']").click
+
+      expect(page).not_to have_text('Edit this tutorial')
+      expect(page).not_to have_text('Delete this tutorial')
     end
   end
 end

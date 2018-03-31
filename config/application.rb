e99ec0f1f64b99f9b84e95f2898f849ed3836e9d
Thomas William Page <tom.w.page@gmail.com>
2018-03-31 20:51:40 +0200

    Add config change

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module TutorialReport
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
   end
 end

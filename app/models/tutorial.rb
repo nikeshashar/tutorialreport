af62c8409b87721224231f8984950a5d6f97b651
Thomas William Page <tom.w.page@gmail.com>
2018-04-20 12:31:07 +0200

    Try new method

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -1,8 +1,8 @@
 class Tutorial < ApplicationRecord
-  has_attached_file :avatar,
-                    styles: { medium: "300x300>", thumb: "100x100>" },
-                    default_url: "/images/:style/missing.png",
-                    :path => ":rails_root/public/system/tutorials/avatars/:id/:style_:basename.:extension",
-                    :url => "/system/tutorials/avatars/:id/:style_:basename.:extension"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/ unless Rails.env.test?
+  # has_attached_file :avatar,
+  #                   styles: { medium: "300x300>", thumb: "100x100>" },
+  #                   default_url: "/images/:style/missing.png",
+  #                   :path => ":rails_root/public/system/tutorials/avatars/:id/:style_:basename.:extension",
+  #                   :url => "/system/tutorials/avatars/:id/:style_:basename.:extension"
+  # validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/ unless Rails.env.test?
 end

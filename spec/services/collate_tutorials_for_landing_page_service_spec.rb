58f2f25fafee59e6911ffb3cd943750aed6c68af
Thomas William Page <tom.w.page@gmail.com>
2018-04-24 14:04:42 +0200

    Add test for random feature
    Co-authored-by: attyc <antoniajcronin@gmail.com>

--- spec/services/collate_tutorials_for_landing_page_service_spec.rb
+++ spec/services/collate_tutorials_for_landing_page_service_spec.rb
@@ -1,25 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe CollateTutorialsForLandingPageService do
- let!(:tutorial1) { create(:tutorial, rating: 4) }
- let!(:tutorial2) { create(:tutorial, rating: 8) }
- let!(:tutorial3) { create(:tutorial, rating: 7) }
- let!(:tutorial4) { create(:tutorial, rating: 9) }
- let!(:tutorial5) { create(:tutorial, rating: 10) }
+  subject { CollateTutorialsForLandingPageService.new }
 
- describe 'when called' do
-   it 'returns correct tutorial' do
-     expect(CollateTutorialsForLandingPageService.new.call).to include Hash[first: tutorial2]
-   end
-   it 'returns 4 correct tutorials (rating 7+)' do
-     expect(CollateTutorialsForLandingPageService.new.call).to eq Hash[
+  let!(:tutorial1) { create(:tutorial, rating: 4) }
+  let!(:tutorial2) { create(:tutorial, rating: 8) }
+  let!(:tutorial3) { create(:tutorial, rating: 7) }
+  let!(:tutorial4) { create(:tutorial, rating: 9) }
+  let!(:tutorial5) { create(:tutorial, rating: 10) }
+
+  describe 'when called' do
+    it 'returns correct tutorial' do
+      expect(subject.call).to include Hash[first: tutorial2]
+    end
+
+    it 'returns 4 correct tutorials (rating 7+)' do
+      expect(subject.call).to eq Hash[
        first: tutorial2,
        second: tutorial3,
        third: tutorial4,
        fourth: tutorial5 ]
-   end
-   it 'does not return incorrect tutorials (rating below 7)' do
-     expect(CollateTutorialsForLandingPageService.new.call).not_to include [tutorial1]
-   end
- end
+    end
+
+    it 'does not return incorrect tutorials (rating below 7)' do
+      expect(subject.call).not_to include [tutorial1]
+    end
+
+    it 'returns random responses' do
+      result1 = subject.call
+      result2 = subject.call
+
+      expect(result1).not_to eq result2
+    end
+  end
 end

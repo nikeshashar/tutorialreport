3c8a1f00f7872c1279a1f596e7fea27477c19915
Thomas William Page <tom.w.page@gmail.com>
2018-04-25 19:29:09 +0200

    Remove user testing

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -2,25 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Signing Up', type: feature do
   describe 'The signing up process' do
-    let(:user_email) { 'example@email.com' }
-    let(:user_password) { '123456' }
-
-    it 'signs the user up' do
-      skip
-      visit '/'
-
-      expect(page).to have_content 'This is the Tutorial Report Landing Page'
-
-      find(:xpath, "//a[@href='/users/sign_up']").click
-
-      within '.new_user' do
-        fill_in 'user_email', with: user_email
-        fill_in 'user_password', with: user_password
-        fill_in 'user_password_confirmation', with: user_password
-        find('[name=commit]').click
-      end
-
-      expect(page).to have_content 'Welcome! You have signed up successfully.'
-    end
+  
   end
 end

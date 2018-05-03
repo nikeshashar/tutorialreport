214e4b9df0666f9d4b5012703a162da2227926ca
Thomas William Page <tom.w.page@gmail.com>
2018-05-03 15:00:41 +0200

    Allow users to add rating

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # devise_for :users
   # resources :tutorials
   get 'tutorials/:id', to: 'tutorials#show'
+  resources :reviews, only: [:create, :new]
 
   root to: "landing_page#index"
 end

c991f3cf4d0607a5fe12331200388a48030734b1
Thomas William Page <tom.w.page@gmail.com>
2018-05-07 18:35:48 +0200

    Remove avatar shiz, Add url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425171508) do
+ActiveRecord::Schema.define(version: 20180507163146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 20180425171508) do
     t.string   "type_of_tutorial"
     t.string   "language"
     t.string   "avatar_url"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

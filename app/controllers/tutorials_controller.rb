4adc9dfce4a5249917832d0af18547809a0e2411
Tom <tom.w.page@gmail.com>
2018-05-08 18:54:49 +0200

    Undo trial

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,5 +1,5 @@
 class TutorialsController < ApplicationController
-  # before_action :authenticate_user!, except: [ :index, :show ]
+  before_action :authenticate_user!, except: [ :index, :show ]
 
   def index
     @tutorials = Tutorial.all

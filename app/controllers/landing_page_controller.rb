06df3dd5180421ed6f8279d7ae33db81f61475f3
Tom <tom.w.page@gmail.com>
2018-05-14 14:27:41 +0200

    Add visibility

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,5 +1,7 @@
 class LandingPageController < ApplicationController
   def index
+    puts "REACHED THIS SHIT YO!!"
     @tutorials = Tutorial.all
+    puts "THIS IS WHAT IT LOOKS LIKE >>>>#{@tutorials}"
   end
 end

eecb90e1a09312b014011115042c5fc866e7e3e4
Tom <tom.w.page@gmail.com>
2018-05-14 14:41:23 +0200

    Remove visual

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,7 +1,5 @@
 class LandingPageController < ApplicationController
   def index
-    puts "REACHED THIS SHIT YO!!"
     @tutorials = Tutorial.all
-    puts "THIS IS WHAT IT LOOKS LIKE >>>>#{@tutorials.first.language.capitalize}"
   end
 end
